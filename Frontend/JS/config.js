db2c1105e956fc34963193189e46e064067701a0
Martin <martintorresgongora21@gmail.com>
2025-11-01 13:17:33 -0500

    Update config.js

--- Frontend/JS/config.js
+++ Frontend/JS/config.js
@@ -6,12 +6,12 @@ const CONFIG = {
   API_URL:
     window.location.hostname === "localhost"
       ? "http://localhost:3000/api" // Nota el /api al final
-      : "https://post-place-backend-ayf9.onrender.com/api", // Y aquí también
+      : "https://laboratorio-anterior.onrender.com/api", // Y aquí también
 
   SOCKET_URL:
     window.location.hostname === "localhost"
       ? "http://localhost:3000"
-      : "https://post-place-backend-ayf9.onrender.com",
+      : "https://laboratorio-anterior.onrender.com",
 };
 
 // Hacer CONFIG global
